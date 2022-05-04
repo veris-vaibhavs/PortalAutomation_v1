28c456b758e6dfbf34330ad3da6a67c7c550eeac
veris-vivekanand <vivek.anand@veris.in>
2022-05-04 18:00:12 +0530

    Added service option to firefox

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,13 +4,14 @@ from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 from selenium.webdriver.chrome.options import Options
 from WebConfig.web_config import TestData
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-@pytest.fixture(params=["chrome"], scope='class')
+@pytest.fixture(params=["firefox"], scope='class')
 def init_driver(request):
     caps = DesiredCapabilities.CHROME
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
@@ -21,7 +22,7 @@ def init_driver(request):
     if request.param == "chrome":
         web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options, desired_capabilities=caps)
     if request.param == "firefox":
-        web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
+        web_driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
     request.cls.driver = web_driver
     web_driver.implicitly_wait(100)
     yield 
