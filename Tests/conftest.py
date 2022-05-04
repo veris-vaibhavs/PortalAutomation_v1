1aba35811e9e1b8de868222bf838edf4ffa3844c
veris-vivekanand <vivek.anand@veris.in>
2022-05-04 17:37:34 +0530

    Added service option to chromedriver

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,7 +1,10 @@
 from http.server import executable
 import pytest
 from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.chrome.options import Options
 from WebConfig.web_config import TestData
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -13,10 +16,10 @@ def init_driver(request):
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
     options = Options()
     options.add_argument('--incognito')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--start-maximized')
     if request.param == "chrome":
-        web_driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options, desired_capabilities=caps)
+        web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options, desired_capabilities=caps)
     if request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
     request.cls.driver = web_driver
