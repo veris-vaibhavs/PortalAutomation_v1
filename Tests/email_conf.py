0eae35df7cada90d454786762c5fb564ab022ada
veris-vivekanand <89376089+veris-vivekanand@users.noreply.github.com>
2022-05-06 13:57:37 +0530

    Update email_conf.py

--- Tests/email_conf.py
+++ Tests/email_conf.py
@@ -6,10 +6,10 @@ from smtplib import SMTP_SSL_PORT
 imaphost = "imap.gmail.com"
 username = "code.tester2021@gmail.com"
 
-app_password = "awdtrwovjtbfkrgt"
+app_password = ""
 
 # Deatils for sending pytest report
 smtp_ssl_host = "smtp.gmail.com"
-smtp_ssl_port = 465
+smtp_ssl_port = ""
 sender = ""
 targets = ["vivek.anand@veris.in"]
