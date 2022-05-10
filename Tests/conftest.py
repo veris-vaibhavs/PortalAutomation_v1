9416445c851b22f47729824fe221579749308233
veris-vaibhavs <vaibhav.srivastava@veris.in>
2022-05-10 16:47:19 +0530

    testesasdsxxxxssg

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -24,6 +24,7 @@ def init_driver(request):
     options.add_argument("--disable-gpu");
     options.add_argument("--disable-extensions");
     options.add_argument("--dns-prefetch-disable");
+    options.add_argument("--disable-dev-shm-usage");
     options.add_argument('--incognito')
     options.add_argument('--headless')
     options.add_argument('--start-maximized')
