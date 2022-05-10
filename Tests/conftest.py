370b3a58e122ba0aeabf42727f70200b0a7bdbc8
veris-vaibhavs <vaibhav.srivastava@veris.in>
2022-05-10 17:04:11 +0530

    testesasdsxxxxssasgss

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -12,19 +12,19 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.utils import ChromeType
 
 
-# @pytest.fixture(params=["chrome"], scope='class')
+@pytest.fixture(params=["chrome"], scope='class')
 def init_driver(request):
     caps = DesiredCapabilities.CHROME
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
     options = Options()
-    options.add_argument("--no-sandbox");
-    options.add_argument("--disable-features=VizDisplayCompositor");
-    options.add_argument("enable-automation");
-    options.add_argument("--window-size=1920,1080");
-    options.add_argument("--disable-gpu");
-    options.add_argument("--disable-extensions");
-    options.add_argument("--dns-prefetch-disable");
-    options.add_argument("--disable-dev-shm-usage");
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument("enable-automation")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--dns-prefetch-disable")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument('--incognito')
     options.add_argument('--headless')
     options.add_argument('--start-maximized')
