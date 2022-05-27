37358d691407ba069d60439a73f85fa7dfea13e2
veris-vivekanand <vivek.anand@veris.in>
2022-05-27 10:07:21 +0530

    Checking multiprocessing2

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -33,9 +33,9 @@ def init_driver(request):
     options.add_experimental_option('useAutomationExtension', False)
     if request.param == "chrome":
     #    web_driver = webdriver.Chrome(executable_path="chromedriver", options=options, desired_capabilities=caps)
-        web_driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options) 
+        # web_driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options) 
  
-    #    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+       web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     if request.param == "firefox":
         web_driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
     request.cls.driver = web_driver
