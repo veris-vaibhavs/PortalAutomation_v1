6325155b0ea74747d0049f8b4da286fcd7173e1e
veris-vivekanand <vivek.anand@veris.in>
2022-06-09 18:15:06 +0530

    testing only 2 funcs at a time

--- scratch.py
+++ scratch.py
@@ -13,33 +13,33 @@ def func2():
     os.system("pytest -v -s --html=report_prw.html -m prw Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
     os.system("pytest -v -s Tests/test_send_email.py")
  
-def func3():
-    os.system("pytest -v -s --html=report_prsc.html -m prsc Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
-    os.system("pytest -v -s Tests/test_send_email.py")
-    os.system("pytest -v -s --html=report_pcnclb.html -m pcnclb Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
-    os.system("pytest -v -s Tests/test_send_email.py")
+# def func3():
+#     os.system("pytest -v -s --html=report_prsc.html -m prsc Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
+#     os.system("pytest -v -s Tests/test_send_email.py")
+#     os.system("pytest -v -s --html=report_pcnclb.html -m pcnclb Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
+#     os.system("pytest -v -s Tests/test_send_email.py")
  
-def func4():
-    os.system("pytest -v -s --html=report_misc.html -m misc Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
-    os.system("pytest -v -s Tests/test_send_email.py")
-    # os.system("pytest -v -s --html=report_hostrltd.html -m hostrltd Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
-    # os.system("pytest -v -s Tests/test_send_email.py")
+# def func4():
+#     os.system("pytest -v -s --html=report_misc.html -m misc Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
+#     os.system("pytest -v -s Tests/test_send_email.py")
+#     # os.system("pytest -v -s --html=report_hostrltd.html -m hostrltd Tests/Positive_Tests/Test_Resource_Booking/test_RoomBooking.py --self-contained-html --capture=sys -rP -rF")
+#     # os.system("pytest -v -s Tests/test_send_email.py")
 
 
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=func1)
     t2 = threading.Thread(target=func2)
-    t3 = threading.Thread(target=func3)
-    t4 = threading.Thread(target=func4)
+    # t3 = threading.Thread(target=func3)
+    # t4 = threading.Thread(target=func4)
 
     t1.start()
     t2.start()
-    t3.start()
-    t4.start()
+    # t3.start()
+    # t4.start()
 
 
     t1.join()
     t2.join()
-    t3.join()
-    t4.join()
+    # t3.join()
+    # t4.join()
